edb05080fcccaad75373b028499bed9b37d1f23d
rajkaranrk164@gmail.com <rajkaranrk164@gmail.com>
2023-12-30 09:53:05 +0530

    first commit related to setup

---
+++ src/main/java/com/rk/hibernate/features/HibernateFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.rk.hibernate.features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateFeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rk/hibernate/features/HibernateFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rk.hibernate.features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
