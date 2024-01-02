569a6ce6b079945e379a86800507a21213b5f7b3
rajkaranrk164@gmail.com <rajkaranrk164@gmail.com>
2024-01-02 10:03:40 +0530

    spring data rest code added for UserReposity interface

---
+++ src/main/java/com/rk/hibernate/features/two/domain/onetomany/User.java
@@ -0,0 +1,32 @@
+package com.rk.hibernate.features.two.domain.onetomany;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "dob")
+    private String dob;
+}
---
+++ src/main/java/com/rk/hibernate/features/two/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.rk.hibernate.features.two.repository;
+
+import com.rk.hibernate.features.two.domain.onetomany.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+ //url : http://localhost:8080/user - POST (create)
+// GET http://localhost:8080/user/{id}
+ @Repository
+ @RepositoryRestResource(path = "user")
+public interface UserRepository extends JpaRepository<User, Long> {
+}
